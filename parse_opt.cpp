244ace439809ccedbd39c81e2a07cf0e78788d1c
Cyrille VALLIET <cvalliet@CV-MBP.lan>
2020-06-06 16:44:44 +0200

    reformat

--- parse_opt.cpp
+++ parse_opt.cpp
@@ -8,50 +8,27 @@ private:
     char** argv_;
 
 public:
-    class iterator: public std::iterator<std::output_iterator_tag, char**, char**, const char ***, char**>
+    class iterator: public std::iterator<std::output_iterator_tag,
+        char**, char**, const char ***, char**>
     {
     private:
         char** p_;
 
     public:
-        explicit iterator(char** p): p_{p}
-        {}
+        explicit iterator(char** p): p_{p} {}
 
-        iterator& operator++()
-        {
-            ++p_;
-            return *this;
-        }
-
-        bool operator==(iterator other) const
-        {
-            return p_ == other.p_;
-        }
-
-        bool operator!=(iterator other) const
-        {
-            return !(*this == other);
-        }
-
-        reference operator*() const
-        {
-            return p_;
-        }
+        iterator& operator++() { ++p_; return *this; }
+        bool operator==(iterator other) const { return p_ == other.p_; }
+        bool operator!=(iterator other) const { return !(*this == other); }
+        reference operator*() const { return p_; }
     };
 
     explicit arguments(int argc, char** argv):
         argc_{argc}, argv_{argv}
     {}
 
-    iterator begin()
-    {
-        return iterator(argv_);
-    }
-
-    iterator end()
-    {
-        return iterator(argv_ + argc_);
-    }
+    iterator begin() { return iterator(argv_); }
+    iterator end() { return iterator(argv_ + argc_); }
 };
 
 int main(int argc, char* argv[])
@@ -64,4 +41,3 @@ int main(int argc, char* argv[])
 
     return EXIT_SUCCESS;
 }
-
