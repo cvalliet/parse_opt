2ab36ac4bd0894dceffa2acb326df0ed7907a42e
Cyrille VALLIET <cvalliet@CV-MBP.lan>
2020-06-08 08:54:51 +0200

    assign option to current arg if set

--- parse_opt.cpp
+++ parse_opt.cpp
@@ -8,19 +8,81 @@ private:
     char** argv_;
 
 public:
-    class iterator: public std::iterator<std::output_iterator_tag,
-        char**, char**, const char ***, char**>
+    class parameter
     {
     private:
         char** p_;
+        const std::string* opt_;
 
     public:
-        explicit iterator(char** p): p_{p} {}
+        parameter(char** p, const std::string* opt = nullptr):
+            p_{p}, opt_{opt}
+        {}
 
-        iterator& operator++() { ++p_; return *this; }
+        const char* content() const
+        {
+            if (is_short_opt())
+                return *p_ + 1;
+
+            if (is_long_opt())
+                return *p_ + 2;
+                
+            return *p_;
+        }
+
+        const char* option() const
+        {
+            if (not opt_)
+                return "";
+
+            return opt_->c_str();
+        }
+
+        bool is_opt() const
+        {
+            return ((*p_)[0] == '-');
+        }
+
+        bool is_short_opt() const
+        {
+            return (((*p_)[0] == '-') && ((*p_)[1] != '-'));
+        }
+
+        bool is_long_opt() const
+        {
+            return (((*p_)[0] == '-') && ((*p_)[1] == '-'));
+        }
+    };
+
+    class iterator: public std::iterator<std::forward_iterator_tag,
+        parameter, parameter, const parameter*, parameter>
+    {
+    private:
+        std::string opt_;
+        char**      p_;
+
+        void parse_option()
+        {
+            if (not *p_)
+                return;
+
+            char *p = *p_;
+
+            if (*p == '-') {
+                ++p;
+                if (*p == '-')
+                    ++p;
+                opt_ = p;
+            }
+        }
+
+    public:
+        explicit iterator(char** p): opt_(), p_{p} {}
+
+        iterator& operator++() { ++p_; parse_option(); return *this; }
         bool operator==(iterator other) const { return p_ == other.p_; }
         bool operator!=(iterator other) const { return !(*this == other); }
-        reference operator*() const { return p_; }
+        reference operator*() const { return parameter(p_, &opt_); }
     };
 
     explicit arguments(int argc, char** argv):
@@ -36,8 +98,47 @@ int main(int argc, char* argv[])
     arguments args(argc, argv);
 
     for (auto arg: args) {
-        std::cout << "Arg : " << *arg << std::endl; 
+        std::string prefix = "arg : ";
+        if (arg.is_short_opt())
+            prefix = "opt : ";
+
+        if (arg.is_long_opt())
+            prefix = "opt : ";
+
+        std::cout << prefix << arg.content() << " " << arg.option() << "\n"; 
     }
 
     return EXIT_SUCCESS;
 }
+
+#if defined(DEPRECATED)
+class cstring
+{
+private:
+    char* c_str_;
+
+public:
+    class iterator: public std::iterator<std::output_iterator_tag,
+        char*, char*, const char **, char*>
+    {
+    private:
+        char* p_;
+
+    public:
+        explicit iterator(char** p): p_{p} {}
+
+        iterator& operator++() { ++p_; return *this; }
+        bool operator==(iterator other) const { return p_ == other.p_; }
+        bool operator!=(iterator other) const { return !(*this == other); }
+        reference operator*() const { return p_; }
+    };
+
+    explicit cstring(char* c_str):
+        c_str_{c_str}
+    {}
+
+    iterator begin() { return iterator(c_str_); }
+    iterator end() { return iterator(c_str_ + strlen(c_str_)); }
+};
+
+#endif
